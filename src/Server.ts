e8d126e4c46ad51fbaa973c92ecf4747c39bddd5
Nazdum <camilo.mendez@uao.edu.co>
2021-12-25 18:18:07 -0500

    configuración del servidor express

---
+++ src/Server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import DefineEnvironment from '../config/environments/';
+
+class Server {
+  private _app: express.Application;
+  constructor() {
+    this._app = express();
+    this.config();
+  }
+
+  private config = (): void => {
+    const defineEnvironment = new DefineEnvironment();
+    const environmentPort = defineEnvironment.getCurrentEnvironment().getPort();
+    this._app.set('port', environmentPort);
+  };
+  start = (): void => {
+    this._app.listen(this._app.get('port'), () => {
+      console.log(`
+      *****\tAPI listening on port ${this._app.get('port')}\t        *****
+      *****\trunning on ${process.env.NODE_ENV} environment\t*****
+      `);
+    });
+  };
+}
+
+export default Server;
